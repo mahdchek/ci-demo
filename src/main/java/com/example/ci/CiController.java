0e183c507566c5264959c92b4bef86332318fde9
mahdi chekini <me.chekini@gmail.com>
2022-01-30 15:35:27 +0100

    Add jenkinsfile

--- src/main/java/com/example/ci/CiController.java
+++ src/main/java/com/example/ci/CiController.java
@@ -2,10 +2,12 @@ package com.example.ci;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("api")
 public class CiController {
 
     private final CiService ciService;
