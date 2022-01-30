1c9ea0104709a653a333fb68f96b39e142aea5b8
mahdi chekini <me.chekini@gmail.com>
2022-01-30 15:58:50 +0100

    take into account sonar code smells

--- src/main/java/com/example/ci/CiRepository.java
+++ src/main/java/com/example/ci/CiRepository.java
@@ -2,7 +2,6 @@ package com.example.ci;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Repository
